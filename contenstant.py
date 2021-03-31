3d9cecc82cb92956d90b74dba5966a3a742f1e0a
RobinHeath-Albuquerque <79651450+RobinHeath-Albuquerque@users.noreply.github.com>
2021-03-31 11:18:54 -0600

    Added methods to register contestant, pick winner and print contestant info to sweepstakes.py

--- contenstant.py
+++ contenstant.py
@@ -1,6 +1,7 @@
 class Contestant:
 
-    def __init__(self, name, email):
+    def __init__(self, name, email, registration_number):
         self.first_name = name
         self.last_name = name
         self.email = email
+        self.registration_number = ()
--- sweepstakes.py
+++ sweepstakes.py
@@ -1,3 +1,27 @@
+import random
+from contenstant import Contestant
 class Sweepstakes:
-    def __init__(self, name):
+    def __init__(self, name, contestant):
         self.name = name
+        self.contestant = ()
+
+
+sweepstakes = {
+    "prize": "Cruise",
+    "contest start date": 11/22/2021,
+    "contest end date": 10/3/2022
+}
+
+
+def register_contestant(self_contestant):
+    print("This contestant has been registered")
+
+
+def pick_winner(contestant):
+    print(random.choice(contestant))
+
+
+def print_contestant_info(contestant):
+    print_contestant_info(contestant)
+
+
